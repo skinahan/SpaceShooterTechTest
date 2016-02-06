502c4106467bc72bea18fb043833c05c312e81a9
Sean Kinahan <skinahan@trinity.edu>
2016-02-06 17:08:43 -0600

    Finished HUD, changed input style to mirror elite:dangerous (sliding thrust value between -100 and 100)

--- SpaceShooterTechTest/Assets/Scripts/PlayerController.cs
+++ SpaceShooterTechTest/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,12 @@ public class PlayerController : MonoBehaviour {
     public Image backwardMovementImage;
     public Image leftMovementImage;
     public Image rightMovementImage;
+    public Image inertiaImage;
+    public Image upMovementImage;
+    public Image downMovementImage;
+
+    public Slider forwardThrustSlider;
+    public Slider backwardThrustSlider;
 
     private Color startColor;
 
@@ -26,29 +32,40 @@ public class PlayerController : MonoBehaviour {
     private bool movingRight = false;
     private bool movingLeft = false;
     private bool movingBackward = false;
+    private bool movingUp = false;
+    private bool movingDown = false;
+
+    private int mainEngineThrust = 0;
 
     void Start()
     {
         startColor = forwardMovementImage.color;
         rb = GetComponent<Rigidbody>();
     }
-	
-    void FixedUpdate ()
+
+    private void checkMovement()
     {
         float forwardVelocity = Vector3.Dot(rb.velocity, transform.forward);
         float rightwardVelocity = Vector3.Dot(rb.velocity, transform.right);
-        Vector3 localangularvelocity = transform.InverseTransformDirection(rb.angularVelocity).normalized * rb.angularVelocity.magnitude;
+        float upwardVelocity = Vector3.Dot(rb.velocity, transform.up);
 
+        //Vector3 localangularvelocity = transform.InverseTransformDirection(rb.angularVelocity).normalized * rb.angularVelocity.magnitude;
         if (forwardVelocity == 0)
         {
             movingForward = false;
             movingBackward = false;
         }
 
-        if(rightwardVelocity == 0)
+        if (rightwardVelocity == 0)
         {
             movingRight = false;
             movingLeft = false;
+        }
+
+        if (upwardVelocity == 0)
+        {
+            movingUp = false;
+            movingDown = false;
         }
 
         if (forwardVelocity > 1)
@@ -60,7 +77,7 @@ public class PlayerController : MonoBehaviour {
             movingForward = false;
         }
 
-        if(forwardVelocity < -1)
+        if (forwardVelocity < -1)
         {
             movingBackward = true;
         }
@@ -69,7 +86,25 @@ public class PlayerController : MonoBehaviour {
             movingBackward = false;
         }
 
-        if(rightwardVelocity > 1)
+        if (upwardVelocity > 1)
+        {
+            movingUp = true;
+        }
+        else
+        {
+            movingUp = false;
+        }
+
+        if (upwardVelocity < -1)
+        {
+            movingDown = true;
+        }
+        else
+        {
+            movingDown = false;
+        }
+
+        if (rightwardVelocity > 1)
         {
             movingRight = true;
         }
@@ -79,86 +114,135 @@ public class PlayerController : MonoBehaviour {
         }
 
         if (rightwardVelocity < -1)
-        {            
+        {
             movingLeft = true;
         }
         else
         {
             movingLeft = false;
         }
+    }
+	
+    void FixedUpdate ()
+    {
+        checkMovement();
 
+        if (Input.GetButtonDown("inertiaDamp"))
+        {
+            inertiaDamping = !inertiaDamping;
+        }
+
+        if(Input.GetButtonDown("cutEngine"))
+        {
+            mainEngineThrust = 0;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Quaternion F = transform.rotation;
+            GameObject.Instantiate(bullet, transform.position + spawnDistance * transform.forward, F * Quaternion.Euler(90, 0, 0));
+        }
 
         if (inertiaDamping)
         {
+            inertiaImage.color = startColor;
             rb.drag = 0.5f;
             rb.angularDrag = 1f;
         }
         else
         {
+            inertiaImage.color = Color.clear;
             rb.drag = 0.2f;
             rb.angularDrag = 0.05f;
         }
-        if (Input.GetButtonDown("inertiaDamp"))
-		{
-            inertiaDamping = !inertiaDamping;
-		}
-        if(Input.GetButtonDown("Fire1"))
+    }
+
+    private void updateIndicator(Image img, bool status)
+    {
+        if(status)
         {
-            Quaternion F = transform.rotation;
-            GameObject.Instantiate(bullet, transform.position + spawnDistance * transform.forward, F * Quaternion.Euler(90,0,0));
+            img.color = startColor;
+        }
+        else
+        {
+            img.color = Color.clear;
         }
     }
 
-	// Update is called once per frame
-	void Update () {    
+    private void updateMovementIndicators()
+    {
+        updateIndicator(forwardMovementImage, movingForward);
+        updateIndicator(backwardMovementImage, movingBackward);
+        updateIndicator(leftMovementImage, movingLeft);
+        updateIndicator(rightMovementImage, movingRight);
+        updateIndicator(upMovementImage, movingUp);
+        updateIndicator(downMovementImage, movingDown);
+    }
 
-        if(movingForward)
+    private void updateHUD()
+    {
+        if(mainEngineThrust == 0)
         {
-            forwardMovementImage.color = startColor;
-        }
-        else
-        {
-            forwardMovementImage.color = Color.clear;
-        }
-
-        if(movingBackward)
-        {
-            backwardMovementImage.color = startColor;
-        }
-        else
-        {
-            backwardMovementImage.color = Color.clear;
+            forwardThrustSlider.value = 0;
+            backwardThrustSlider.value = 0;
         }
 
-        if(movingLeft)
-        {
-            leftMovementImage.color = startColor;
-        }
-        else
-        {
-            leftMovementImage.color = Color.clear;
-        }
+        if(mainEngineThrust > 0)
+            forwardThrustSlider.value = mainEngineThrust;
 
-        if (movingRight)
-        {
-            rightMovementImage.color = startColor;
-        }
-        else
-        {
-            rightMovementImage.color = Color.clear;
-        }
+        if(mainEngineThrust < 0)
+            backwardThrustSlider.value = -mainEngineThrust;
 
+        updateMovementIndicators();
+    }
+
+    private void handleInput()
+    {
         float h = Input.GetAxis("Horizontal") * strafeThrust * Time.deltaTime;
         float v = Input.GetAxis("Vertical") * mainThrust * Time.deltaTime;
-
         float clockwise = Input.GetAxis("Clockwise") * rotateThrust * Time.deltaTime;
-
         float x = Input.GetAxis("Jump") * verticalThrust * Time.deltaTime;        
+
 
         rb.AddForce(transform.forward * v, ForceMode.Acceleration);
         rb.AddForce(transform.right * h, ForceMode.Acceleration);
         rb.AddForce(transform.up * x, ForceMode.Acceleration);
-
         rb.AddTorque(-transform.forward * clockwise);
+    }
+
+    private void alternateInput()
+    {
+        float h = Input.GetAxis("Horizontal") * strafeThrust * Time.deltaTime;
+
+        float v = Input.GetAxis("Vertical");
+
+        if(v > 0)
+        {
+            if(mainEngineThrust < 100)
+                mainEngineThrust++;
+        }
+
+        if(v < 0)
+        {
+            if(mainEngineThrust > -100)
+                mainEngineThrust--;
+        }
+
+        float clockwise = Input.GetAxis("Clockwise") * rotateThrust * Time.deltaTime;
+
+        float x = Input.GetAxis("Jump") * verticalThrust * Time.deltaTime;
+
+
+        rb.AddForce(transform.forward * mainEngineThrust, ForceMode.Acceleration);
+        rb.AddForce(transform.right * h, ForceMode.Acceleration);
+        rb.AddForce(transform.up * x, ForceMode.Acceleration);
+        rb.AddTorque(-transform.forward * clockwise);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        updateHUD();
+        //handleInput();
+        alternateInput();
     }
 }
